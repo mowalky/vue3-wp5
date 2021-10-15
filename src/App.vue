64a4654b483f521aba9f85fbaee645dbac538c5e
mowalky <dev@mowalky.com>
2021-10-15 07:31:53 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+<div class="m-4 text-3xl font-bold">
+  Hello from tailwind
+</div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+
+  export default defineComponent({});
+</script>
